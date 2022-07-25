a030cb6bd7d06d57a67f9671b3f9ab600813b4e1
vvnha <vvnha.17it3@sict.udn.vn>
2022-07-25 15:00:39 +0700

    first commit

--- js/constants.js
+++ js/constants.js
@@ -1,17 +1,17 @@
 export const TURN = {
-  CROSS: "cross",
-  CIRCLE: "circle",
+  CROSS: 'cross',
+  CIRCLE: 'circle',
 };
 
 export const CELL_VALUE = {
-  CROSS: "X",
-  CIRCLE: "O",
-  WIN: "win",
+  CROSS: 'X',
+  CIRCLE: 'O',
+  WIN: 'win',
 };
 
 export const GAME_STATUS = {
-  PLAYING: "PLAYING",
-  ENDED: "END",
-  X_WIN: "X",
-  O_WIN: "O",
+  PLAYING: 'PLAYING',
+  ENDED: 'END',
+  X_WIN: 'X WIN',
+  O_WIN: 'O WIN',
 };
