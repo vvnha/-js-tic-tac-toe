0d600306a91d9e77b3cd83f1f66437cabaaf19d7
vvnha <vvnha.17it3@sict.udn.vn>
2022-07-25 14:13:18 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -5,17 +5,63 @@ import {
   getGameStatusElement,
 } from './selectors.js';
 
-console.log(getCellElementAtIdx(3));
-console.log(getCellElementList());
-console.log(getCurrentTurnElement());
-console.log(getGameStatusElement());
+import { GAME_STATUS, TURN } from './constants.js';
+import { checkGameStatus } from './utils.js';
 
 /**
  * Global variables
  */
-let currentTurn = 'cross';
+let currentTurn = TURN.CROSS;
 let isGameEnded = false;
 let cellValues = new Array(9).fill('');
+
+function toggleTurn() {
+  currentTurn = currentTurn === TURN.CIRCLE ? TURN.CROSS : TURN.CIRCLE;
+}
+
+function handleCellClick(cell, index) {
+  // set selected cell
+  const isSelected =
+    cell.classList.contains(TURN.CROSS) || cell.classList.contains(TURN.CIRCLE);
+
+  if (isSelected) return;
+
+  const result = checkGameStatus(currentTurn, index);
+  const statusElement = getGameStatusElement();
+
+  if (statusElement) {
+    statusElement.textContent = result.status;
+  }
+
+  const currentTurnElement = getCurrentTurnElement();
+
+  if (currentTurnElement) {
+    currentTurnElement.classList.remove(TURN.CIRCLE, TURN.CROSS);
+    currentTurnElement.classList.add(
+      currentTurn === TURN.CIRCLE ? TURN.CROSS : TURN.CIRCLE
+    );
+  }
+
+  cell.classList.add(currentTurn);
+  cell.dataset.value = currentTurn;
+  // change curren turn
+  toggleTurn();
+}
+
+function initCellElementList() {
+  const cellElementList = getCellElementList();
+
+  cellElementList.forEach((cell, index) => {
+    cell.addEventListener('click', () => handleCellClick(cell, index));
+  });
+}
+
+(() => {
+  //bind click even for all element
+  initCellElementList();
+  //bind click even for replay button
+  //...
+})();
 
 /**
  * TODOs
