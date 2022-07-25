320c07b9ff9eeeb947dffa48f558c34074ba92b3
vvnha <vvnha.17it3@sict.udn.vn>
2022-07-25 16:06:08 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -19,31 +19,58 @@ let cellValues = new Array(9).fill('');
 function toggleTurn() {
   currentTurn = currentTurn === TURN.CIRCLE ? TURN.CROSS : TURN.CIRCLE;
 }
+function updateGameStatus(status) {
+  const statusElement = getGameStatusElement();
+
+  if (statusElement) {
+    statusElement.textContent = status;
+  }
+}
+
+function showReplayButton() {
+  const replayButtonElement = getReplayButtonElement();
+  if (isGameEnded && replayButtonElement) {
+    replayButtonElement.style.display = 'inline-block';
+  }
+}
+
+function highlightWinCells(winIndexList) {
+  if (!Array.isArray(winIndexList) && winIndexList.length > 0) return;
+
+  for (let i = 0; i < winIndexList.length; i++) {
+    const cellElement = getCellElementAtIdx(winIndexList[i]);
+    cellElement.style.backgroundColor = 'rgba(255, 255, 255, 0.1)';
+  }
+}
 
 function handleCellClick(cell, index) {
   // set selected cell
   const isSelected =
     cell.classList.contains(TURN.CROSS) || cell.classList.contains(TURN.CIRCLE);
 
-  if (isSelected) return;
+  if (isSelected || isGameEnded) return;
 
   cellValues[index] =
     currentTurn === 'cross' ? CELL_VALUE.CROSS : CELL_VALUE.CIRCLE;
   const result = checkGameStatus(cellValues);
-  const replayButtonElement = getReplayButtonElement();
-  isGameEnded =
-    result.status === GAME_STATUS.ENDED ||
-    result.status === GAME_STATUS.O_WIN ||
-    result.status === GAME_STATUS.X_WIN;
 
-  if (isGameEnded && replayButtonElement) {
-    replayButtonElement.style.display = 'inline-block';
-  }
+  switch (result.status) {
+    case GAME_STATUS.O_WIN:
+    case GAME_STATUS.X_WIN:
+      isGameEnded = true;
+      updateGameStatus(result.status);
+      showReplayButton();
+      highlightWinCells(result.winPositions);
+      break;
 
-  const statusElement = getGameStatusElement();
+    case GAME_STATUS.ENDED:
+      isGameEnded = true;
+      updateGameStatus(result.status);
+      showReplayButton();
+      break;
 
-  if (statusElement) {
-    statusElement.textContent = result.status;
+    default:
+      break;
   }
 
   const currentTurnElement = getCurrentTurnElement();
@@ -75,6 +102,7 @@ function initReplayButton() {
 
   replayButtonElement.addEventListener('click', () => {
     cellValues = new Array(9).fill('');
+    isGameEnded = false;
     cellElementList.forEach((cell) => {
       cell.classList.remove(TURN.CROSS, TURN.CIRCLE);
     });
